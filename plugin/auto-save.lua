4cc6835d96ee02c9effa4f2faf6ea92c5b2ebbc1
benlubas <benmlubas@gmail.com>
2023-11-17 21:46:53 -0500

    axe

--- plugin/auto-save.lua
+++ plugin/auto-save.lua
@@ -1,15 +1,11 @@
 if vim.g.loaded_auto_save then
   return
 end
+
 vim.g.loaded_auto_save = true
 
-local command = vim.api.nvim_create_user_command
-local cnf = require("auto-save.config").opts
-
-command("ASToggle", function()
-	require("auto-save").toggle()
+vim.api.nvim_create_user_command("ASToggle", function()
+  require("auto-save").toggle()
 end, {})
 
-if cnf.enabled then
-	require("auto-save").on()
-end
+require("auto-save").on()
